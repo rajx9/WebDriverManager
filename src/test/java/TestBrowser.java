5a57f6642f188e6584c2b6d24af5b73ccaa4b147
Kumar <2095302@cognizant.com>
2022-06-22 02:31:51 +0530

    initial commit

---
+++ src/test/java/TestBrowser.java
@@ -0,0 +1,34 @@
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBrowser {
+	public static String Browser="edge"; //Excel sheet /config.properties 
+	public static WebDriver driver;
+	
+	public static void main(String[] args) {
+		
+		if(Browser.equalsIgnoreCase("chrome")){
+			WebDriverManager.chromedriver().setup();
+			 driver=new ChromeDriver();
+		}
+		else if(Browser.equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			 driver=new EdgeDriver();
+		}
+		else if(Browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			 driver=new FirefoxDriver();
+		}
+		
+		driver.get("https://www.google.com");
+		driver.close();
+
+	}
+
+}
